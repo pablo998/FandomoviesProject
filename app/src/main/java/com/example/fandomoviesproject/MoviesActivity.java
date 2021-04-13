8b2ba058adf328824df08dde55ae2ac6aaee9d51
Fran <fran.felipe2320@gmail.com>
2021-04-13 21:19:48 +0100

    creando activity y clase de peliculas de una categoria

--- app/src/main/java/com/example/fandomoviesproject/MoviesActivity.java
+++ app/src/main/java/com/example/fandomoviesproject/MoviesActivity.java
@@ -1,6 +1,7 @@
 package com.example.fandomoviesproject;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -9,31 +10,34 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
-public class MoviesCategoryActivity extends AppCompatActivity {
+public class MoviesActivity extends AppCompatActivity {
 
     RecyclerView recycler;
     ArrayList<String> listDatos;
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.category_movies);
+        setContentView(R.layout.movies_activity);
 
-        recycler=(RecyclerView) findViewById(R.id.recyclerView);
+        recycler=(RecyclerView) findViewById(R.id.recyclerViewMovies);
         recycler.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
 
         listDatos = new ArrayList<String>();
 
         for(int i=0; i<=10; i++){
-            listDatos.add("Categoria " + i);
+            listDatos.add("Pelicula  " + i);
         }
 
         MoviesAdapter adapter = new MoviesAdapter(listDatos);
         recycler.setAdapter(adapter);
 
-        Toolbar toolbar = findViewById(R.id.toolbarcategory);
-        toolbar.setTitle(R.string.movies);
+        Toolbar toolbar = findViewById(R.id.toolbarmovies);
+        toolbar.setTitle(R.string.peliculas);
+
+        text = findViewById(R.id.textCategoriaElegida);
+        text.setText(R.string.categoria_elegida);
 
     }
 }
-
