397ca0014ab6cd6003daf4cf09bfd0e94b69a387
pedro gil <pederitoo@gmail.com>
2021-04-14 09:15:16 +0100

    cambios menores layout detalle pelicula

--- app/src/main/java/com/example/fandomoviesproject/activity_detalle_pelicula.java
+++ app/src/main/java/com/example/fandomoviesproject/activity_detalle_pelicula.java
@@ -1,6 +1,8 @@
 package com.example.fandomoviesproject;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +12,14 @@ public class activity_detalle_pelicula extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalle_pelicula);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Detalle Pelicula");
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(false);
+            actionBar.hide();
+        }
     }
 }
