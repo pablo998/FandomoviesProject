24a64c6eb374678990046c67468fab78b0d882fe
pedro gil <pederitoo@gmail.com>
2021-05-16 04:56:12 +0100

    Implementado Navigation Drawer en pantalla ayuda

--- app/src/main/java/com/example/fandomoviesproject/ayuda/AyudaActivity.java
+++ app/src/main/java/com/example/fandomoviesproject/ayuda/AyudaActivity.java
@@ -1,33 +1,58 @@
 package com.example.fandomoviesproject.ayuda;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.example.fandomoviesproject.R;
+import com.example.fandomoviesproject.compras.ComprasActivity;
+import com.example.fandomoviesproject.favoritos.FavoritosActivity;
+import com.example.fandomoviesproject.menu.MenuActivity;
+import com.example.fandomoviesproject.perfil.perfilActivity;
+import com.google.android.material.navigation.NavigationView;
 
 
-
-public class AyudaActivity extends AppCompatActivity implements AyudaContract.View {
+public class AyudaActivity extends AppCompatActivity implements AyudaContract.View, NavigationView.OnNavigationItemSelectedListener {
     AyudaContract.Presenter presenter;
 
     private Context context = this;
     TextView informacionText;
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ayuda);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar = findViewById(R.id.main_toolbar);
         setSupportActionBar(toolbar);
 
         informacionText = findViewById(R.id.ayuda_text);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toogle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.nav_drawer_open, R.string.nav_drawer_close);
+        drawerLayout.addDrawerListener(toogle);
+        toogle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav_help);
 
 
         ActionBar actionBar = getSupportActionBar();
@@ -62,4 +87,42 @@ public class AyudaActivity extends AppCompatActivity implements AyudaContract.Vi
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.nav_home:
+                Intent intent = new Intent(this, MenuActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_perfil:
+                Intent intent2 = new Intent(this, perfilActivity.class);
+                startActivity(intent2);
+                break;
+            case R.id.nav_fav:
+                Intent intent3 = new Intent(this, FavoritosActivity.class);
+                startActivity(intent3);
+                break;
+            case R.id.nav_cart:
+                Intent intent4 = new Intent(this, ComprasActivity.class);
+                startActivity(intent4);
+                break;
+            case R.id.nav_help:
+                Intent intent5 = new Intent(this, AyudaActivity.class);
+                startActivity(intent5);
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
+
+        //presenter.onBackPressed();
+    }
 }
