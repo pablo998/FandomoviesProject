efb7cf8ca2b727e80cc5db03c6b8899ee4987381
pedro gil <pederitoo@gmail.com>
2021-05-16 04:12:59 +0100

    implementado Navigation Drawer en pantalla categorias de un documental

--- app/src/main/java/com/example/fandomoviesproject/categoriasDocu/CategoryDocuListActivity.java
+++ app/src/main/java/com/example/fandomoviesproject/categoriasDocu/CategoryDocuListActivity.java
@@ -10,29 +10,41 @@ import android.widget.ListView;
 
 //import android.widget.Toolbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.example.fandomoviesproject.R;
+import com.example.fandomoviesproject.ayuda.AyudaActivity;
+import com.example.fandomoviesproject.compras.ComprasActivity;
 import com.example.fandomoviesproject.data.CategoryDocuItemCatalog;
+import com.example.fandomoviesproject.favoritos.FavoritosActivity;
+import com.example.fandomoviesproject.perfil.perfilActivity;
+import com.google.android.material.navigation.NavigationView;
 
 
 public class CategoryDocuListActivity
-        extends AppCompatActivity implements CategoryDocuListContract.View {
+        extends AppCompatActivity implements CategoryDocuListContract.View, NavigationView.OnNavigationItemSelectedListener {
 
     public static String TAG = CategoryDocuListActivity.class.getSimpleName();
 
     CategoryDocuListContract.Presenter presenter;
 
     private ListView listView;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categorias_de_documentales);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar = findViewById(R.id.main_toolbar);
         setSupportActionBar(toolbar);
 
 
@@ -49,6 +61,17 @@ public class CategoryDocuListActivity
         CategoryDocuListScreen.configure(this);
 
         presenter.fetchCategoryListData();
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toogle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.nav_drawer_open, R.string.nav_drawer_close);
+        drawerLayout.addDrawerListener(toogle);
+        toogle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+
     }
 
     @Override
@@ -103,6 +126,38 @@ public class CategoryDocuListActivity
     }
 
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.nav_home:
+                Intent intent = new Intent(this, perfilActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_fav:
+                Intent intent2 = new Intent(this, FavoritosActivity.class);
+                startActivity(intent2);
+                break;
+            case R.id.nav_cart:
+                Intent intent3 = new Intent(this, ComprasActivity.class);
+                startActivity(intent3);
+                break;
+            case R.id.nav_help:
+                Intent intent4 = new Intent(this, AyudaActivity.class);
+                startActivity(intent4);
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
 
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
 
+        //presenter.onBackPressed();
+    }
 }
